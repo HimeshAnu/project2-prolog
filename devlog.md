d14a1a7f63a0074d849b325919c8688316e797bb
Himesh A <himesha@mac.local.ftr>
2026-05-08 13:56:56 -0500

    Finished up the makeshift4 and assignstation5

--- devlog.md
+++ devlog.md
@@ -1,2 +1,5 @@
-5/8/2026 
+5/8/2026  11:32am 
 Added the main prolog files and the other main files. Began by adding the plan/1 predicate. It will create a plan/3 structure that has the morning, evening and night schedules. It will also collect all employees and pass them through the shifts so that every employee can eventually be scheduled once. 
+
+5/8/2026 1:52 pm
+Added the make_shift/4 predicate. This part finds all the workstations that are active during a shift. I also implemented the assign_stations/5 which assigns employees to every active workstation. This took a lot longer than I had planned for and will likely continue working on this until I finish the project. 
--- project2.pl
+++ project2.pl
@@ -3,3 +3,18 @@ plan(plan(Morning, Evening, Night)) :-
     make_shift(morning, Employees, AfterMorning, Morning),
     make_shift(evening, AfterMorning, AfterEvening, Evening),
     make_shift(night, AfterEvening, [], Night).
+
+make_shift(Shift, Available, Remaining, Schedule) :-
+    findall(ws(Station, Min, Max),
+            (workstation(Station, Min, Max), \+ workstation_idle(Station, Shift)),
+            Stations),
+    assign_stations(Stations, Shift, Available, Remaining, Schedule).
+
+assign_stations([], _, Available, Available, []).
+
+assign_stations([ws(Station, Min, Max)|RestStations], Shift, Available, Remaining,
+                [workstation(Station, Workers)|RestSchedule]) :-
+    Min =< Max,
+    between(Min, Max, Count),
+    choose_valid_workers(Count, Available, Shift, Station, Workers, AfterStation),
+    assign_stations(RestStations, Shift, AfterStation, Remaining, RestSchedule).
